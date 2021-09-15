71c9447fa121aef4510d8eb4f5c04039561859f5
Heimir Kristmundsson <heimir.97rgmail.com>
2021-09-15 14:58:36 +0200

    Register and login now working, not the most beautiful solution

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,27 @@
-const loginView = document.getElementById("login_view");
-const login_button = document.getElementById("login_button").addEventListener("click", toggleLoginView(loginView));
-const close_login = document.getElementById("close_login").addEventListener("click", toggleLoginView(loginView));
+const login_button = document.getElementById("login_button").addEventListener("click", toggleLoginView);
+const close_login = document.getElementById("close_login").addEventListener("click", toggleLoginView);
 
 
-const register_view = document.getElementById("register_view").addEventListener("click", toggleLoginView);
-const register_button = document.getElementById("register_button").addEventListener("click", toggleLoginView);
-
-function toggleLoginView(view) {
-    if (view.style.display === "none") {
-        view.style.display = "block";
+function toggleLoginView() {
+    const loginView = document.getElementById("login_view");
+    if (loginView.style.display === "none") {
+        loginView.style.display = "block";
     } else {
-        view.style.display = "none";
+        loginView.style.display = "none";
     }
-  }
+  } 
+
+
+
+const register_button = document.getElementById("register_button").addEventListener("click", toggleRegisterView);
+const close_register = document.getElementById("close_register").addEventListener("click", toggleRegisterView);
+
+
+function toggleRegisterView() {
+    const registerView = document.getElementById("register_view");
+    if (registerView.style.display === "none") {
+        registerView.style.display = "block";
+    } else {
+        registerView.style.display = "none";
+    }
+  } 
