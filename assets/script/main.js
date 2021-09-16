63f4dcea6bef280a13dfb0750503d17a4b3a3a17
Heimir Kristmundsson <heimir.97rgmail.com>
2021-09-16 07:54:01 +0200

    Added title to index.html

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,27 +1,26 @@
 const login_button = document.getElementById("login_button").addEventListener("click", toggleLoginView);
 const close_login = document.getElementById("close_login").addEventListener("click", toggleLoginView);
+const loginView = document.getElementById("login_view");
 
 
 function toggleLoginView() {
-    const loginView = document.getElementById("login_view");
     if (loginView.style.display === "none") {
         loginView.style.display = "block";
     } else {
         loginView.style.display = "none";
     }
-  } 
+} 
 
 
 
 const register_button = document.getElementById("register_button").addEventListener("click", toggleRegisterView);
 const close_register = document.getElementById("close_register").addEventListener("click", toggleRegisterView);
-
+const registerView = document.getElementById("register_view");
 
 function toggleRegisterView() {
-    const registerView = document.getElementById("register_view");
     if (registerView.style.display === "none") {
         registerView.style.display = "block";
     } else {
         registerView.style.display = "none";
     }
-  } 
+} 
