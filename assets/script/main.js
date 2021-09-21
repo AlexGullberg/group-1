f595f3e546a8ea086ad3d17f242750323f838ff0
Heimir Kristmundsson <heimir.97rgmail.com>
2021-09-21 13:05:21 +0200

    added fading to open and close login and register

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,62 @@
-const login_button = document.getElementById("login_button").addEventListener("click", toggleLoginView);
-const close_login = document.getElementById("close_login").addEventListener("click", toggleLoginView);
 const loginView = document.getElementById("login_view");
-
-
-function toggleLoginView() {
-    if (loginView.style.display === "none") {
-        loginView.style.display = "block";
-    } else {
-        loginView.style.display = "none";
-    }
-} 
-
-
-
-const register_button = document.getElementById("register_button").addEventListener("click", toggleRegisterView);
-const close_register = document.getElementById("close_register").addEventListener("click", toggleRegisterView);
 const registerView = document.getElementById("register_view");
 
-function toggleRegisterView() {
-    if (registerView.style.display === "none") {
-        registerView.style.display = "block";
-    } else {
-        registerView.style.display = "none";
-    }
+const login_button = document.getElementById("login_button").addEventListener("click", function() {
+    toggleViews(loginView)
+});
+const close_login = document.getElementById("close_login").addEventListener("click", function() {
+    toggleViews(loginView)
+});
+
+const register_button = document.getElementById("register_button").addEventListener("click", function() {
+    toggleViews(registerView)
+});
+
+const close_register = document.getElementById("close_register").addEventListener("click", function() {
+    toggleViews(registerView)
+});
+
+
+
+function toggleViews(view) {
+    view.classList.toggle("login-view-toggle")
+}
+
+
+
+
+
+/*
+function toggleLoginView() {
+    const loginView = document.getElementById("login_view");
+    loginView.classList.toggle("login-view-toggle")
 } 
+
+*/
+/*
+function toggleLoginView() {
+    if (loginView.style.visibility === "hidden") {
+        loginView.style.visibility = "visible";
+        loginView.style.opacity = "1";
+    } else {
+        loginView.style.visibility = "hidden";
+    }
+} */
+
+
+
+
+
+/*
+function toggleRegisterView() {
+    registerView.classList.toggle("login-view-toggle");
+}
+*/
+/*
+function toggleRegisterView() {
+    if (registerView.style.visibility === "hidden") {
+        registerView.style.visibility = "visible";
+    } else {
+        registerView.style.visibility = "hidden";
+    }
+} */
