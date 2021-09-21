7808be3ec9b049b4fef4bbefa9f1ccdf6421556d
Savi Malakit <savitri.malakit@gmail.com>
2021-09-21 17:33:41 +0200

    tried to add numbers in the pie

--- assets/script/pie.js
+++ assets/script/pie.js
@@ -1,21 +1,43 @@
 let ctx = document.getElementById("myChart").getContext("2d");
-let labels = ["lön","csn","bidrag","rånbyte"];
-let colorHex = ["#BDE4DD","rgba(255,184,0, 0.5)","#DDE6FB","#FF977C"];
+let labels = ["lön", "csn", "bidrag", "rånbyte"];
+let colorHex = ["#BDE4DD", "rgba(255,184,0, 0.5)", "#DDE6FB", "#FF977C"];
 
 let myChart = new Chart(ctx, {
-    type: "pie",
-    data: {
-        datasets: [{
-            data: [15,10,5,70],
-            backgroundColor: colorHex
-        }],
-        labels: labels
+  type: "pie",
+  data: {
+    datasets: [
+      {
+        data: [15, 10, 5, 70],
+        backgroundColor: colorHex,
+      },
+    ],
+    labels: labels,
+  },
+  options: {
+    responsive: true,
+    legend: {
+      position: "bottom",
     },
-    options:{
-        responsive:true,
-        legend: {
-            position: "bottom"
-        }
-    }
-
-})
+    plugins: {
+      datalabels: {
+        color: "#000000",
+        anchor: "end",
+        align: "start",
+        offset: -10,
+        borderWidth: 2,
+        borderColor: "#000000",
+        borderRadius: 25,
+        backgroundColor: (context) => {
+          return context.dataset.backgroundColor;
+        },
+        font: {
+          weight: "bold",
+          size: "10",
+        },
+        formatter: (value) => {
+          return value + "%";
+        },
+      },
+    },
+  },
+});
