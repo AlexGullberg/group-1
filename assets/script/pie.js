829469fa3f65fe2a54d9703e08fbc56a3cb26f1e
Heimir Kristmundsson <heimir.97rgmail.com>
2021-09-23 12:41:51 +0200

    JS

--- assets/script/pie.js
+++ assets/script/pie.js
@@ -1,14 +1,26 @@
+let labels = [];
+let data = [];
+
+function getPercentageAndCategories(percentage) {
+  //chart.update(); //update after new data
+  labels
+  data
+}
+
+
+
 let ctx = document.getElementById("myChart").getContext("2d");
 
-let labels = ["Lön", "CSN", "Bidrag", "Rånbyte"];
+// let labels = ["Lön", "CSN", "Bidrag", "Rånbyte"];
 let colorHex = ["#BDE4DD", "rgba(255,184,0, 0.5)", "#DDE6FB", "#FF977C"];
+
 
 let myChart = new Chart(ctx, {
   type: "pie",
   data: {
     datasets: [
       {
-        data: [15, 10, 5, 70],
+        data: [15, 10, 5, 77.85698644801829],
         backgroundColor: colorHex,
       },
     ],
